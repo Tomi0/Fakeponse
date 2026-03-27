03feee1e984b6f7a5deca38eeace8fbd21a867d6
Tomi0 <tomasbuendiaalacid@gmail.com>
2026-03-27 23:09:12 +0100

    Setting up project for development

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+su user -c "composer dump-autoload --optimize"
+su user -c "php artisan optimize:clear"
+
 if [ "${APP_ENV}" == "production" ]; then
-    su user -c "composer dump-autoload --optimize"
-    su user -c "php artisan optimize:clear"
     su user -c "php artisan optimize"
     su user -c "php artisan migrate --force"
 fi
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json(['hola' => 'mundo']);
+    return response()->json([
+        'hola' => 'mundo',
+        'config' => config('app.name'),
+    ]);
 });
